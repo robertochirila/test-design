1ebebf3bccf4638940609c549aad28705436ae3c
Roberto Chirila <robertochirila@robertos-mbp.home>
2020-06-28 23:44:20 +0100

    added flexbox grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="wrapper">
+          <div className="wrapper-box">
+            <p>First Cool Paragraph</p>
+            <div className="wrapper-column">
+              <p>Column Paragraph</p>
+            </div>
+            <div className="wrapper-column">
+              <p>Column Paragraph</p>
+            </div>
+            <div className="wrapper-column">
+              <p>Column Paragraph</p>
+            </div>
+          </div>
+          <div className="wrapper-box">
+            <p>Second Cool Paragraph</p>
+            <div className="wrapper-column">
+              <p>Column Paragraph</p>
+            </div>
+            <div className="wrapper-column">
+              <p>Column Paragraph</p>
+            </div>
+            <div className="wrapper-column">
+              <p>Column Paragraph</p>
+            </div>
+          </div>
+          <div className="wrapper-box">
+            <p>Third Cool Paragraph</p>
+            <div className="wrapper-column">
+              <p>Column Paragraph</p>
+            </div>
+            <div className="wrapper-column">
+              <p>Column Paragraph</p>
+            </div>
+            <div className="wrapper-column">
+              <p>Column Paragraph</p>
+            </div>
+          </div>
+          <div className="wrapper-box">
+            <p>Forth Cool Paragraph</p>
+            <div className="wrapper-column">
+              <p>Column Paragraph</p>
+            </div>
+            <div className="wrapper-column">
+              <p>Column Paragraph</p>
+            </div>
+            <div className="wrapper-column">
+              <p>Column Paragraph</p>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
